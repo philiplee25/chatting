61b6bcfe62a9c9332c896c2310765273efcb6543
leepl <yplunap25@gmail.com>
2023-07-14 17:00:06 +0900

    spinner, streamBuilder

--- lib/screens/chatScreen.dart
+++ lib/screens/chatScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -34,23 +35,46 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Chat screen'),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.exit_to_app_sharp,
+        appBar: AppBar(
+          title: Text('Chat screen'),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.exit_to_app_sharp,
+              ),
+              onPressed: () {
+                _authentication.signOut();
+                Navigator.pop(context);
+              },
             ),
-            onPressed: () {
-              _authentication.signOut();
-              Navigator.pop(context);
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text('Chat screen'),
-      ),
-    );
+          ],
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('chats/TgG6J7pUxJKjhOVfCxFK/message')
+              .snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            final docs = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    docs[index]['text'],
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                );
+              },
+            );
+          },
+        ));
   }
 }
