98d9e8b54c4ad894790dde069bcc9729e43a75f6
leepl <yplunap25@gmail.com>
2023-07-18 23:05:21 +0900

    chat message, firestore

---
+++ lib/chatting/chat/message.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages extends StatelessWidget {
+  const Messages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy(
+            'time',
+            descending: true,
+          )
+          .snapshots(),
+      builder: (context,
+          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final chatDocs = snapshot.data!.docs;
+
+        return ListView.builder(
+          reverse: true,
+          itemCount: chatDocs.length,
+          itemBuilder: (context, index) {
+            return Text(chatDocs[index]['text']);
+          },
+        );
+      },
+    );
+  }
+}
